a42d867aaeac8ae184bd4cf9a702762e20e27a2b
Laurel Ash <laurel.ash@proton.me>
2024-01-01 15:16:31 -0500

    Migrating archive management code to kbmarc

--- src/kbm/__init__.py
+++ src/kbm/__init__.py
@@ -95,28 +95,4 @@ class SettingsParser:
         log.debug("Execution value: %s", excinst)
         log.debug("Traceback: %s", exctb)
 
-class BackupManager:
-    # I'm pretty sure I only want each instance to have access to its own settings
-    def __init__(self, archive_name):
-        pass
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        pass
-
-class Archiver(BackupManager):
-    def __init__(self, archive_name, archive_type):
-        super().__init__(profile_name)
-        self.archive_type = archive_type
-
-    def __repr__(self):
-        return f"Archiver('{self.archive_name}', '{self.archive_type}')"
-
-class Unarchiver(BackupManager):
-    def __init__(self, profile_name, archive_type):
-        super().__init__(profile_name)
-        self.archive_type = archive_type
-
 
