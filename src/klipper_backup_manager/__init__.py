3e12e4405008db9d4451b5e05fce4ec005ac2688
Laurel Ash <laurel.ash@proton.me>
2024-01-02 16:32:37 -0500

    I'm so tired.

--- src/klipper_backup_manager/__init__.py
+++ src/klipper_backup_manager/__init__.py
@@ -2,4 +2,23 @@
 #
 
 # SPDX-License-Identifier: GPL-3.0-or-later
+import click, cloup
+import kbm
+import click
+from cloup import (
+    HelpFormatter, HelpTheme, Style, command, group, option, option_group, argument
+    )
+from cloup.constraints import mutually_exclusive, RequireAtLeast
+from kbm import SettingsParser
+log = kbm.log.getChild(__name__)
 
+formatter_settings=HelpFormatter.settings(
+    theme=HelpTheme(
+        invoked_command=Style(fg='bright_yellow'),
+        heading=Style(fg='bright_white', bold=True),
+        constraint=Style(fg='magenta'),
+        col1 = Style(fg='bright_yellow')
+    )
+)
+
+# Need to implement entire command structure for cloup handling. Deal with this later.
