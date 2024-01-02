78650d379a15c496d011aab51ac48c07760b54f9
Laurel Ash <laurel.ash@proton.me>
2024-01-02 16:06:22 -0500

    Prepping command structure for branching off.

--- src/kbm/__init__.py
+++ src/kbm/__init__.py
@@ -1,8 +1,4 @@
 # SPDX-FileCopyrightText: 2023-present Laurel Ash <laurel.ash@proton.me>
-# SPDX-FileCopyrightText: 2023-present Laurel Ash <laurel.ash@proton.me>
-#
-
-
 # SPDX-License-Identifier: GPL-3.0-or-later
 import logging
 import os
