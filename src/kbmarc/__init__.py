ea6049870c4aed157e6aeec93e606c5b9dcd4fb2
Laurel Ash <laurel.ash@proton.me>
2024-01-01 19:51:47 -0500

    Basic methods for opening/closing files. Need to add error handling.

--- src/kbmarc/__init__.py
+++ src/kbmarc/__init__.py
@@ -22,18 +22,10 @@ class ArchiveManager:
             self.arc_cfg = s[self.arc_type]
         self.recents = self.arc_cfg['recents']
         self.file_path = os.path.join(kbm.kbmlocal, self.recents[self.how_recent]['location'])
-        if not tarfile.is_tarfile(self.file_path) or not os.path.exists(self.file_path):
-            log.exception('Error validating file %s', self.file_path)
-            raise FileNotFoundError('File does not exist or is not a tar archive.')
-
     def __enter__(self):
-        self.file = tarfile.open(self.file_path, 'r')
-        return self.file
-    
+        return self.file_path
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        tarfile.close()
-
-
+        pass
 
 class Archiver(ArcManager):
     def __init__(self, arc_type, how_recent=0, cmode='xz'):
@@ -44,15 +36,24 @@ class Archiver(ArcManager):
         self.file = tarfile.open(self.file_path, f'w:{self.cmode}')
         return self.file
     def __exit__(self, exc_type, exc_value, exc_traceback):
+        log.debug('Closing file %s', self.file.name)
         self.file.close()
-
-
-    def __repr__(self):
-        return f"Archiver('{self.arc_type}', 'how_recent={self.how_recent}', 'cmode={self.cmode})"
-
-
+        log.debug('Exiting Unarchiver context manager.')
+        log.debug('Exception type:: %s', exc_type)
+        log.debug('Value: %s', exc_value)
+        log.debug('Traceback: %s', exc_traceback)
+        
 class Unarchiver(ArcManager):
-    def __init__(self, arc_type, cmode, is_local=1, how_recent=0):
+    def __init__(self, arc_type, how_recent=0):
         super().__init__(arc_type, how_recent)
         self.name = 'Unarchiver'
-        self.archive_type = archive_type
+    def __enter__(self):
+        self.file = tarfile.open(self.file_path, 'r')
+        return self.file
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        log.debug('Closing file %s', self.file.name)
+        self.file.close() # assuming nothing has gone wrong up to this point
+        log.debug('Exiting Unarchiver context manager.')
+        log.debug('Exception type:: %s', exc_type)
+        log.debug('Value: %s', exc_value)
+        log.debug('Traceback: %s', exc_traceback)
