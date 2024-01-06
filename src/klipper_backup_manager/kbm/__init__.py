ef8be68e3c0f2faf446787a5c58e9415e0ff62ff
Laurel Ash <laurel.ash@proton.me>
2024-01-05 22:16:24 -0500

    aaaaaaaaaa

--- src/klipper_backup_manager/kbm/__init__.py
+++ src/klipper_backup_manager/kbm/__init__.py
@@ -9,7 +9,7 @@ from pathlib import Path
 def file_timestamp():
     return datetime.now().astimezone().strftime("%Y-%m-%d_%H%M%S")
 
-scripthome = Path(os.path.realpath(__file__)).parent.resolve()
+scripthome = Path(os.path.realpath(__file__)).parent.parent.resolve()
 userhome = os.path.expanduser("~")
 kbmlocal = os.path.join(userhome, ".kbmlocal")
 logdir = os.path.join(os.path.expanduser("~/.kbmlocal"), "logs")
@@ -19,7 +19,6 @@ kbm_yaml = os.path.join(os.path.expanduser("~/.kbmlocal"), "kbm.yaml")
 log = logging.getLogger(__name__)
 if not os.path.exists(kbmdefault_yaml):
     log.critical('Failed to find %s', kbmdefault_yaml)
-    raise FileNotFoundError
 if not os.path.exists(kbm_yaml):
     shutil.copy(kbmdefault_yaml, kbmlocal)
 
