1e8d65b9a9c511f354ba9c3a2c58ec0ee2670e8f
Laurel Ash <laurel.ash@proton.me>
2024-01-05 22:16:24 -0500

    aaaaaaaaaa

--- src/klipper_backup_manager/__about__.py
+++ src/klipper_backup_manager/__about__.py
@@ -1,3 +1,3 @@
 # SPDX-FileCopyrightText: 2023-present Laurel Ash <laurel.ash@proton.me>
 # SPDX-License-Identifier: GPL-3.0-or-later
-__version__ = "0.13.1b0"
+__version__ = "0.13.1b1"
--- src/klipper_backup_manager/kbm/__init__.py
+++ src/klipper_backup_manager/kbm/__init__.py
@@ -5,20 +5,49 @@ import os
 import shutil
 from datetime import datetime
 from pathlib import Path
+import sys
+import yaml
 
 def file_timestamp():
     return datetime.now().astimezone().strftime("%Y-%m-%d_%H%M%S")
 
-scripthome = Path(os.path.realpath(__file__)).parent.parent.resolve()
 userhome = os.path.expanduser("~")
 kbmlocal = os.path.join(userhome, ".kbmlocal")
 logdir = os.path.join(os.path.expanduser("~/.kbmlocal"), "logs")
 backupdir = os.path.join(os.path.expanduser("~/.kbmlocal"), "backups")
-kbmdefault_yaml = os.path.join(scripthome, ".kbmdefault.yaml")
+kbmdefault_yaml = os.path.join(userhome, ".kbmdefault.yaml")
+if not os.path.isfile(kbmdefault_yaml):
+    with open(kbmdefault_yaml, 'w') as file:
+        yamlsettings = {
+            'default':{
+                    'printer':{ 
+                    'name': 'Ender 3',
+                    'printer_data': '~/printer_data'
+                    },
+                'config': {
+                    'location': 'config',
+                    'recent': [],
+                    'maxbackups': 5
+                    },
+                'database':{
+                    'location': 'database', 
+                    'recent': [], 
+                    'maxbackups': 5
+                    }, 
+                'gcodes': {
+                    'location': 'gcodes',
+                    'recent': [], 
+                    'maxbackups': 5
+                    }, 
+                'remote': {
+                    'enabled': False, 
+                    'handler': 'rclone', 
+                    'rclone':'gdrive', 
+                    'path': '.kbmremote'}}}
+    yaml.safe_dump(yamlsettings, file)
+
 kbm_yaml = os.path.join(os.path.expanduser("~/.kbmlocal"), "kbm.yaml")
 log = logging.getLogger(__name__)
-if not os.path.exists(kbmdefault_yaml):
-    log.critical('Failed to find %s', kbmdefault_yaml)
 if not os.path.exists(kbm_yaml):
     shutil.copy(kbmdefault_yaml, kbmlocal)
 
