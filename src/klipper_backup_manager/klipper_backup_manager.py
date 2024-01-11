ef2b2e441551356410f6ae74d8c041018a5f64c2
Laurel Ash <laurel.ash@proton.me>
2024-01-11 11:27:20 -0500

    hey it works! kinda

--- src/klipper_backup_manager/klipper_backup_manager.py
+++ src/klipper_backup_manager/klipper_backup_manager.py
@@ -1,5 +1,39 @@
 #!/usr/bin/env python3
 import kbm
+import sys
+import cloup
+from cloup import option_group, option
+from cloup.constraints import mutually_exclusive, RequireAtLeast
+
+@cloup.command()
+@option_group("Archive options:",
+    option(
+        "--backup", "-b", is_flag=True,
+        help="Backs up your selected target."
+    ),
+    option(
+        "--restore", "-r", is_flag=True,
+        help="Restores your selected target."
+    ),
+    constraint=mutually_exclusive
+)
+@option_group(
+    "Target options:",
+    option(
+        "--config","-c", is_flag=True, help="Backs up Klipper configuration files."
+    ),
+    option(
+        "--gcode", "-g", is_flag=True, help="Backs up gcode files."
+        ),
+        constraint=mutually_exclusive
+)
+def cli(backup, restore, config, gcode):
+    if backup:
+        kbm.backup(mode=("config" if config else "gcode"))
+        sys.exit(0)
+    if restore:
+        kbm.restore(mode=("config" if config else "gcode"))
+        sys.exit(0)
 
 if __name__ == "__main__":
-    kbm.cli()
+    cli()
